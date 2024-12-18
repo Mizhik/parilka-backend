c80ed731aa0fbb7749783495eba0f801d221de48
Mizhik <vlad.mezhinskiy07@gmail.com>
2024-12-19 00:24:19 +0200

    feat: add models(users and orders)

--- app/models/enums.py
+++ app/models/enums.py
@@ -1,1 +1,22 @@
-#this file is for enums
+from enum import Enum
+
+
+class Role(Enum):
+    USER = "user"
+    ADMIN = "admin"
+    WORKER = "worker"
+
+class Status(Enum):
+    NEW = "new"
+    PAID = "paid"
+    ACCEPTED = "accepted"
+    COMPLETED = "completed"
+    CANCELED = "canceled"
+
+class Payment(Enum):
+    CARD = 'card'
+    CASH = 'cash'
+
+class Delivery(Enum):
+    NOVAPOSHTA = 'novaposhta'
+    UKRPOSHTA = 'ukrposhta'
