8f2ac9b63e5fd7995c16a360508406cdbfcb3834
Halyna Rohatska <rohatskahr@gmail.com>
2025-02-12 13:01:31 +0200

    added func "get_popular"

--- app/repository/product.py
+++ app/repository/product.py
@@ -1,3 +1,7 @@
+from typing import Optional
+
+from sqlalchemy.future import select
+
 from app.models.models import Product
 from app.repository.base_repository import BaseRepository
 
@@ -5,3 +9,13 @@ from app.repository.base_repository import BaseRepository
 class ProductRepository(BaseRepository):
     def __init__(self, db):
         super().__init__(db=db, model=Product)
+
+    async def get_popular(
+        self, offset: Optional[int] = None, limit: Optional[int] = None
+    ):
+        stmt = select(self.model).where(self.model.is_popular.is_(True))
+        if offset is not None and limit is not None:
+            stmt = stmt.offset(offset).limit(limit)
+        result = await self.db.execute(stmt)
+        return result.scalars().all()
+
