ced2c85e18b0032bf7114ebd6b7cc2eab044c560
Roasteg <krostislav468@gmail.com>
2025-04-02 12:04:33 +0300

    Protect docs

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from fastapi.responses import JSONResponse
 import uvicorn
 
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import Depends, FastAPI, HTTPException, Request
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi.openapi.utils import get_openapi
 
+from app.core import settings
 from app.core.settings import config
 from app.routes import auth, categories, healthchecker, products
 from app.schemas.response import ResponseSchema
-from app.services.errors import BaseError
+from app.services.errors import BaseError, LoginFailed
 
-app = FastAPI()
+app = FastAPI(docs_url=None)
 
 
 app.add_middleware(
@@ -36,6 +40,24 @@ async def exception_handler(req: Request, ex: HTTPException):
         status_code=ex.status_code,
         content=content
     )
+
+
+security = HTTPBasic()
+
+def get_current_user(credentials: HTTPBasicCredentials = Depends(security)):
+    username = settings.config.DOCS_USER
+    password = settings.config.DOCS_PASSWORD
+    if not(credentials.username == username and credentials.password == password):
+        raise LoginFailed()
+    return credentials.username
+
+@app.get("/docs")
+async def get_docs(_: str = Depends(get_current_user)):
+    return get_swagger_ui_html(openapi_url="/openapi.json", title="docs")
+
+@app.get("/openapi.json")
+async def get_oapi(_: str = Depends(get_current_user)):
+    return get_openapi(title = "FastAPI", version="0.1.0", routes=app.routes)
 
 if __name__ == "__main__":
     uvicorn.run(
