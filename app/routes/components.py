7b697405767782cfc85b7390b4f89e971dca6afc
Halyna Rohatska <rohatskahr@gmail.com>
2025-05-02 18:38:17 +0300

    added filters to components endpoint

--- app/routes/components.py
+++ app/routes/components.py
@@ -1,19 +1,20 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query
 from typing import List, Optional
 
+from typing_extensions import Annotated
+
+from app.schemas.filters import ProductFilterParams
 from app.schemas.product import ProductSchema
 from app.schemas.response import ResponseSchema
 from app.services.components import ComponentsService
 from app.services.dependencies import get_components_service
-
 
 router = APIRouter(prefix="/components", tags=["Components"])
 
 
 @router.get("", response_model=ResponseSchema[List[ProductSchema]])
 async def get_components(
-    offset: Optional[int] = None,
-    limit: Optional[int] = None,
+    filters: Annotated[ProductFilterParams, Query()],
     components_service: ComponentsService = Depends(get_components_service)
 ):
-    return await components_service.get_components(offset=offset, limit=limit)
+    return await components_service.get_components(**filters.dict(exclude_none=True))
--- app/services/components.py
+++ app/services/components.py
@@ -3,6 +3,7 @@ from typing import Optional
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.repository.components import ComponentsRepository
+from app.schemas.filters import ProductFilterParams
 from app.schemas.response import ResponseSchema
 from app.utils.mappers import map_product_to_schema
 
@@ -12,7 +13,13 @@ class ComponentsService:
         self.db = db
         self.repository = repository
 
-    async def get_components(self, offset: Optional[int] = None, limit: Optional[int] = None):
-        components = await self.repository.get_components(offset=offset, limit=limit)
+    async def get_components(self, filters: ProductFilterParams):
+        components = await self.repository.get_components(
+            offset=filters.offset,
+            limit=filters.limit,
+            min_price=filters.min_price,
+            max_price=filters.max_price,
+            manufacturer_ids=filters.manufacturer_id,
+        )
         components_schema = [map_product_to_schema(component) for component in components]
         return ResponseSchema(data=components_schema, message="All components products")
